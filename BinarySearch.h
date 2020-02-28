13ea14b205b172ed0d78191bc4d7ec28df6bf500
Jairun Diemert <54635185+JairunDiemert@users.noreply.github.com>
2020-02-28 12:11:52 -0600

    Create BinarySearch.h

---
+++ BinarySearch.h
@@ -0,0 +1,20 @@
+// BinarySearch.h
+#ifndef BinarySearch_H
+#define BinarySearch_H
+
+class BinarySearch
+{
+  private:
+    int min = 1; 
+    int max = 1000000;
+    int center;
+    int num2Search;
+    int cmpCount = 0;
+    bool go = true;
+
+public:
+  search(int x);
+
+};
+
+#endif
